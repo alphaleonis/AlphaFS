f9296d34dbb8b84d83e6c7e071e9724a83d49095
alphaleonis <alphaleonis@users.noreply.github.com>
2015-01-04 12:09:47 +0100

    Updated names in copyright messages in source code.

--- AlphaFS.UnitTest/FileInfoTest.cs
+++ AlphaFS.UnitTest/FileInfoTest.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright (c) 2008-2015 Peter Palotas, Jeffrey Jangli, Normalex
+﻿/* Copyright 2008-2015 Peter Palotas, Jeffrey Jangli, Alexandr Normuradov
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
