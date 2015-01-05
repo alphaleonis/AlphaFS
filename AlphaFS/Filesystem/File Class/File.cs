68df4ac49863e2c884ef6ab50f9305f41b78274d
Peter Palotas <alphaleonis@users.noreply.github.com>
2015-01-05 20:54:58 +0100

    Extracted File.GetFileSystemEntryInfo methods.

--- AlphaFS/Filesystem/File Class/File.cs
+++ AlphaFS/Filesystem/File Class/File.cs
@@ -44,69 +44,6 @@ namespace Alphaleonis.Win32.Filesystem
    [SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling")]
    public static partial class File
    {
-     
-
-      
-
-      
-
-      
-
-      #region GetFileSystemEntry
-
-      #region IsFullPath
-
-      /// <summary>[AlphaFS] Gets the <see cref="FileSystemEntryInfo"/> of the file on the path.</summary>
-      /// <returns>The <see cref="FileSystemEntryInfo"/> instance of the file or directory.</returns>
-      /// <param name="path">The path to the file or directory.</param>
-      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
-      [SecurityCritical]
-      public static FileSystemEntryInfo GetFileSystemEntryInfo(string path, PathFormat pathFormat)
-      {
-         return GetFileSystemEntryInfoInternal(null, path, false, pathFormat);
-      }
-
-      #endregion // IsFullPath
-
-      /// <summary>[AlphaFS] Gets the <see cref="FileSystemEntryInfo"/> of the file on the path.</summary>
-      /// <returns>The <see cref="FileSystemEntryInfo"/> instance of the file or directory.</returns>
-      /// <param name="path">The path to the file or directory.</param>
-      [SecurityCritical]
-      public static FileSystemEntryInfo GetFileSystemEntryInfo(string path)
-      {
-         return GetFileSystemEntryInfoInternal(null, path, false, PathFormat.Auto);
-      }
-
-      #region Transacted
-
-      #region IsFullPath
-
-      /// <summary>[AlphaFS] Gets the <see cref="FileSystemEntryInfo"/> of the file on the path.</summary>
-      /// <returns>The <see cref="FileSystemEntryInfo"/> instance of the file or directory.</returns>
-      /// <param name="transaction">The transaction.</param>
-      /// <param name="path">The path to the file or directory.</param>
-      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
-      [SecurityCritical]
-      public static FileSystemEntryInfo GetFileSystemEntryInfo(KernelTransaction transaction, string path, PathFormat pathFormat)
-      {
-         return GetFileSystemEntryInfoInternal(transaction, path, false, pathFormat);
-      }
-
-      #endregion // IsFullPath
-
-      /// <summary>[AlphaFS] Gets the <see cref="FileSystemEntryInfo"/> of the file on the path.</summary>
-      /// <returns>The <see cref="FileSystemEntryInfo"/> instance of the file or directory.</returns>
-      /// <param name="transaction">The transaction.</param>
-      /// <param name="path">The path to the file or directory.</param>
-      [SecurityCritical]
-      public static FileSystemEntryInfo GetFileSystemEntryInfo(KernelTransaction transaction, string path)
-      {
-         return GetFileSystemEntryInfoInternal(transaction, path, false, PathFormat.Auto);
-      }
-
-      #endregion // Transacted
-
-      #endregion // GetFileSystemEntry
 
       #region GetLinkTargetInfo
 
@@ -922,33 +859,7 @@ namespace Alphaleonis.Win32.Filesystem
 
       
 
-      #region GetFileSystemEntryInfoInternal
-
-      /// <summary>[AlphaFS] Unified method GetFileSystemEntryInfoInternal() to get a FileSystemEntryInfo from a Non-/Transacted directory/file.</summary>
-      /// <returns>The <see cref="FileSystemEntryInfo"/> instance of the file or directory, or <c>null</c> on Exception when <paramref name="continueOnException"/> is <c>true</c>.</returns>
-      /// <exception cref="ArgumentException">The path parameter contains invalid characters, is empty, or contains only white spaces.</exception>
-      /// <exception cref="ArgumentNullException">path is <c>null</c>.</exception>
-      /// <exception cref="NativeError.ThrowException()"/>
-      /// <param name="transaction">The transaction.</param>
-      /// <param name="path">The path to the file or directory.</param>
-      /// <param name="continueOnException">
-      ///    <para><c>true</c> suppress any Exception that might be thrown a result from a failure,</para>
-      ///    <para>such as ACLs protected directories or non-accessible reparse points.</para>
-      /// </param>
-      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
-      [SecurityCritical]
-      internal static FileSystemEntryInfo GetFileSystemEntryInfoInternal(KernelTransaction transaction, string path, bool continueOnException, PathFormat pathFormat)
-      {
-         // // Enable BasicSearch and LargeCache by default.
-         var directoryEnumerationOptions = DirectoryEnumerationOptions.BasicSearch | DirectoryEnumerationOptions.LargeCache;
-
-         if (continueOnException)
-            directoryEnumerationOptions |= DirectoryEnumerationOptions.ContinueOnException;
-
-         return (new FindFileSystemEntryInfo(false, transaction, path, Path.WildcardQuestion, directoryEnumerationOptions, typeof(FileSystemEntryInfo), pathFormat)).Get<FileSystemEntryInfo>();
-      }
-
-      #endregion // GetFileSystemEntryInfoInternal
+      
 
       #region GetLinkTargetInfoInternal
 
