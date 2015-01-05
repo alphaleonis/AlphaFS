679b64894c6486d4e8f033cfb20a2c2a9aa2575d
Peter Palotas <alphaleonis@users.noreply.github.com>
2015-01-05 20:24:21 +0100

    Extracted File.SetAttributes

---
+++ AlphaFS/Filesystem/File Class/File.SetAttributes.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace Alphaleonis.Win32.Filesystem
+{
+   public static partial class File
+   {
+      #region SetAttributes
+
+      /// <summary>Sets the specified <see cref="FileAttributes"/> of the file or directory on the specified path.</summary>
+      /// <remarks>
+      ///   Certain file attributes, such as <see cref="FileAttributes.Hidden"/> and <see cref="FileAttributes.ReadOnly"/>, can be combined.
+      ///   Other attributes, such as <see cref="FileAttributes.Normal"/>, must be used alone.
+      /// </remarks>
+      /// <remarks>
+      ///   It is not possible to change the <see cref="FileAttributes.Compressed"/> status of a File object using this method.
+      /// </remarks>
+      /// <param name="path">The path to the file or directory.</param>
+      /// <param name="fileAttributes">A bitwise combination of the enumeration values.</param>
+      /// <overloads>Sets the specified <see cref="FileAttributes"/> of the file or directory on the specified path.</overloads>
+      [SecurityCritical]
+      public static void SetAttributes(string path, FileAttributes fileAttributes)
+      {
+         SetAttributesInternal(false, null, path, fileAttributes, false, PathFormat.Auto);
+      }
+
+      /// <summary>[AlphaFS] Sets the specified <see cref="FileAttributes"/> of the file or directory on the specified path.</summary>
+      /// <remarks>
+      ///   Certain file attributes, such as <see cref="FileAttributes.Hidden"/> and <see cref="FileAttributes.ReadOnly"/>, can be combined.
+      ///   Other attributes, such as <see cref="FileAttributes.Normal"/>, must be used alone.
+      /// </remarks>
+      /// <remarks>
+      ///   It is not possible to change the <see cref="FileAttributes.Compressed"/> status of a File object using this method.
+      /// </remarks>
+      /// <param name="path">The path to the file or directory.</param>
+      /// <param name="fileAttributes">A bitwise combination of the enumeration values.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>      
+      [SecurityCritical]
+      public static void SetAttributes(string path, FileAttributes fileAttributes, PathFormat pathFormat)
+      {
+         SetAttributesInternal(false, null, path, fileAttributes, false, pathFormat);
+      }
+
+
+      #region Transacted
+
+      /// <summary>[AlphaFS] Sets the specified <see cref="FileAttributes"/> of the file on the specified path.</summary>
+      /// <remarks>
+      ///   Certain file attributes, such as <see cref="FileAttributes.Hidden"/> and <see cref="FileAttributes.ReadOnly"/>, can be combined.
+      ///   Other attributes, such as <see cref="FileAttributes.Normal"/>, must be used alone.
+      /// </remarks>
+      /// <remarks>
+      ///   It is not possible to change the <see cref="FileAttributes.Compressed"/> status of a File object using this method.
+      /// </remarks>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The path to the file.</param>
+      /// <param name="fileAttributes">A bitwise combination of the enumeration values.</param>      
+      [SecurityCritical]
+      public static void SetAttributes(KernelTransaction transaction, string path, FileAttributes fileAttributes)
+      {
+         SetAttributesInternal(false, transaction, path, fileAttributes, false, PathFormat.Auto);
+      }
+
+      /// <summary>[AlphaFS] Sets the specified <see cref="FileAttributes"/> of the file on the specified path.</summary>
+      /// <remarks>
+      ///   Certain file attributes, such as <see cref="FileAttributes.Hidden"/> and <see cref="FileAttributes.ReadOnly"/>, can be combined.
+      ///   Other attributes, such as <see cref="FileAttributes.Normal"/>, must be used alone.
+      /// </remarks>
+      /// <remarks>
+      ///   It is not possible to change the <see cref="FileAttributes.Compressed"/> status of a File object using this method.
+      /// </remarks>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The path to the file.</param>
+      /// <param name="fileAttributes">A bitwise combination of the enumeration values.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>      
+      [SecurityCritical]
+      public static void SetAttributes(KernelTransaction transaction, string path, FileAttributes fileAttributes, PathFormat pathFormat)
+      {
+         SetAttributesInternal(false, transaction, path, fileAttributes, false, pathFormat);
+      }
+
+      #endregion // Transacted
+
+      #endregion // SetAttributes
+
+      #region Internal Methods
+
+      /// <summary>[AlphaFS] Unified method SetAttributesInternal() to set the attributes for a Non-/Transacted file/directory.</summary>
+      /// <remarks>
+      ///   Certain file attributes, such as <see cref="FileAttributes.Hidden"/> and <see cref="FileAttributes.ReadOnly"/>, can be combined.
+      ///   Other attributes, such as <see cref="FileAttributes.Normal"/>, must be used alone.
+      /// </remarks>
+      /// <remarks>
+      ///   It is not possible to change the <see cref="FileAttributes.Compressed"/> status of a File object using the SetAttributes method.
+      /// </remarks>
+      /// <exception cref="ArgumentException">Thrown when one or more arguments have unsupported or illegal values.</exception>
+      /// <param name="isFolder">Specifies that <paramref name="path"/> is a file or directory.</param>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The name of the file or directory whose attributes are to be set.</param>
+      /// <param name="fileAttributes">
+      ///   The attributes to set for the file or directory. Note that all other values override <see cref="FileAttributes.Normal"/>.
+      /// </param>
+      /// <param name="continueOnNotExist">
+      ///   <see langword="true"/> does not throw an Exception when the file system object does not exist.
+      /// </param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>      
+      [SecurityCritical]
+      internal static void SetAttributesInternal(bool isFolder, KernelTransaction transaction, string path, FileAttributes fileAttributes, bool continueOnNotExist, PathFormat pathFormat)
+      {
+         string pathLp = Path.GetExtendedLengthPathInternal(transaction, path, pathFormat, GetFullPathOptions.RemoveTrailingDirectorySeparator | GetFullPathOptions.CheckInvalidPathChars | GetFullPathOptions.CheckAdditional);
+
+         if (!(transaction == null || !NativeMethods.IsAtLeastWindowsVista
+
+            // SetFileAttributes()
+            // In the ANSI version of this function, the name is limited to MAX_PATH characters.
+            // To extend this limit to 32,767 wide characters, call the Unicode version of the function and prepend "\\?\" to the path.
+            // 2013-01-13: MSDN confirms LongPath usage.
+
+            ? NativeMethods.SetFileAttributes(pathLp, fileAttributes)
+            : NativeMethods.SetFileAttributesTransacted(pathLp, fileAttributes, transaction.SafeHandle)))
+         {
+            if (continueOnNotExist)
+               return;
+
+            uint lastError = (uint)Marshal.GetLastWin32Error();
+
+            switch (lastError)
+            {
+               // MSDN: .NET 3.5+: ArgumentException: FileSystemInfo().Attributes
+               case Win32Errors.ERROR_INVALID_PARAMETER:
+                  throw new ArgumentException(Resources.InvalidFileAttribute);
+
+               case Win32Errors.ERROR_FILE_NOT_FOUND:
+                  if (isFolder)
+                     lastError = (int)Win32Errors.ERROR_PATH_NOT_FOUND;
+
+                  // MSDN: .NET 3.5+: DirectoryNotFoundException: The specified path is invalid, (for example, it is on an unmapped drive).
+                  // MSDN: .NET 3.5+: FileNotFoundException: The file cannot be found.
+                  NativeError.ThrowException(lastError, pathLp);
+                  break;
+            }
+
+            NativeError.ThrowException(lastError, pathLp);
+         }
+      }
+
+      #endregion // SetAttributesInternal
+   }
+}
