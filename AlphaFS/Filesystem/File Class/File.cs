664be540285542859f442d32d2df7cc8df0989d4
Peter Palotas <alphaleonis@users.noreply.github.com>
2015-01-05 20:36:32 +0100

    Extracted File.AddStream methods.

--- AlphaFS/Filesystem/File Class/File.cs
+++ AlphaFS/Filesystem/File Class/File.cs
@@ -44,75 +44,6 @@ namespace Alphaleonis.Win32.Filesystem
    [SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling")]
    public static partial class File
    {
-
-
-      
-
-     
-
-
-      #region AlphaFS
-
-      #region AddStream
-
-      #region IsFullPath
-
-      /// <summary>[AlphaFS] Adds an alternate data stream (NTFS ADS) to an existing file.</summary>
-      /// <param name="path">The path to an existing file.</param>
-      /// <param name="name">The name for the stream. If a stream with <paramref name="name"/> already exists, it will be overwritten.</param>
-      /// <param name="contents">The lines to add to the stream.</param>
-      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>      
-      [SecurityCritical]
-      public static void AddStream(string path, string name, string[] contents, PathFormat pathFormat)
-      {
-         AlternateDataStreamInfo.AddStreamInternal(false, null, path, name, contents, pathFormat);
-      }
-
-      #endregion // IsFullPath
-
-      /// <summary>[AlphaFS] Adds an alternate data stream (NTFS ADS) to an existing file.</summary>
-      /// <param name="path">The path to an existing file.</param>
-      /// <param name="name">The name for the stream. If a stream with <paramref name="name"/> already exists, it will be overwritten.</param>
-      /// <param name="contents">The lines to add to the stream.</param>      
-      [SecurityCritical]
-      public static void AddStream(string path, string name, string[] contents)
-      {
-         AlternateDataStreamInfo.AddStreamInternal(false, null, path, name, contents, PathFormat.Auto);
-      }
-
-      #region Transacted
-
-      #region IsFullPath
-
-      /// <summary>[AlphaFS] Adds an alternate data stream (NTFS ADS) to an existing file.</summary>
-      /// <param name="transaction">The transaction.</param>
-      /// <param name="path">The path to an existing file.</param>
-      /// <param name="name">The name for the stream. If a stream with <paramref name="name"/> already exists, it will be overwritten.</param>
-      /// <param name="contents">The lines to add to the stream.</param>
-      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>      
-      [SecurityCritical]
-      public static void AddStream(KernelTransaction transaction, string path, string name, string[] contents, PathFormat pathFormat)
-      {
-         AlternateDataStreamInfo.AddStreamInternal(false, transaction, path, name, contents, pathFormat);
-      }
-
-      #endregion // IsFullPath
-
-      /// <summary>[AlphaFS] Adds an alternate data stream (NTFS ADS) to an existing file.</summary>
-      /// <param name="transaction">The transaction.</param>
-      /// <param name="path">The path to an existing file.</param>
-      /// <param name="name">The name for the stream. If a stream with <paramref name="name"/> already exists, it will be overwritten.</param>
-      /// <param name="contents">The lines to add to the stream.</param>      
-      [SecurityCritical]
-      public static void AddStream(KernelTransaction transaction, string path, string name, string[] contents)
-      {
-         AlternateDataStreamInfo.AddStreamInternal(false, transaction, path, name, contents, PathFormat.Auto);
-      }
-
-      #endregion Transacted
-
-      #endregion // AddStream
-
       #region Compress
 
       #region IsFullPath
@@ -2163,6 +2094,5 @@ namespace Alphaleonis.Win32.Filesystem
 
       #endregion // Unified Internals
 
-      #endregion // AlphaFS
    }
 }
