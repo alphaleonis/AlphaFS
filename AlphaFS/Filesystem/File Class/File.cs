c9e049af65d0461e3459238cd131c18a870f0211
Peter Palotas <alphaleonis@users.noreply.github.com>
2015-01-05 20:31:16 +0100

    Extracted File.WriteAllBytes

--- AlphaFS/Filesystem/File Class/File.cs
+++ AlphaFS/Filesystem/File Class/File.cs
@@ -46,89 +46,7 @@ namespace Alphaleonis.Win32.Filesystem
    {
 
 
-      #region WriteAllBytes
-
-      #region .NET
-
-      /// <summary>
-      ///   Creates a new file, writes the specified byte array to the file, and then closes the file. If the target file already exists, it is
-      ///   overwritten.
-      /// </summary>
-      /// <param name="path">The file to write to.</param>
-      /// <param name="bytes">The bytes to write to the file.</param>
-      [SuppressMessage("Microsoft.Naming", "CA1720:IdentifiersShouldNotContainTypeNames", MessageId = "bytes")]
-      [SecurityCritical]
-      public static void WriteAllBytes(string path, byte[] bytes)
-      {
-         WriteAllBytesInternal(null, path, bytes, PathFormat.Auto);
-      }
-
-      #endregion // .NET
-
-      #region AlphaFS
-
-      #region IsFullPath
-
-      /// <summary>
-      ///   [AlphaFS] Creates a new file, writes the specified byte array to the file, and then closes the file. If the target file already
-      ///   exists, it is overwritten.
-      /// </summary>
-      /// <param name="path">The file to write to.</param>
-      /// <param name="bytes">The bytes to write to the file.</param>
-      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
-      [SuppressMessage("Microsoft.Naming", "CA1720:IdentifiersShouldNotContainTypeNames", MessageId = "bytes")]
-      [SecurityCritical]
-      public static void WriteAllBytes(string path, byte[] bytes, PathFormat pathFormat)
-      {
-         WriteAllBytesInternal(null, path, bytes, pathFormat);
-      }
-
-      #endregion // IsFullPath
-
-      #region Transacted
-
-      #region .NET
-
-      /// <summary>
-      ///   [AlphaFS] Creates a new file, writes the specified byte array to the file, and then closes the file. If the target file already
-      ///   exists, it is overwritten.
-      /// </summary>
-      /// <param name="transaction">The transaction.</param>
-      /// <param name="path">The file to write to.</param>
-      /// <param name="bytes">The bytes to write to the file.</param>
-      [SuppressMessage("Microsoft.Naming", "CA1720:IdentifiersShouldNotContainTypeNames", MessageId = "bytes")]
-      [SecurityCritical]
-      public static void WriteAllBytes(KernelTransaction transaction, string path, byte[] bytes)
-      {
-         WriteAllBytesInternal(transaction, path, bytes, PathFormat.Auto);
-      }
-
-      #endregion // .NET
-
-      #region IsFullPath
-
-      /// <summary>
-      ///   [AlphaFS] Creates a new file, writes the specified byte array to the file, and then closes the file. If the target file already
-      ///   exists, it is overwritten.
-      /// </summary>
-      /// <param name="transaction">The transaction.</param>
-      /// <param name="path">The file to write to.</param>
-      /// <param name="bytes">The bytes to write to the file.</param>
-      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
-      [SuppressMessage("Microsoft.Naming", "CA1720:IdentifiersShouldNotContainTypeNames", MessageId = "bytes")]
-      [SecurityCritical]
-      public static void WriteAllBytes(KernelTransaction transaction, string path, byte[] bytes, PathFormat pathFormat)
-      {
-         WriteAllBytesInternal(transaction, path, bytes, pathFormat);
-      }
-
-      #endregion // IsFullPath
-
-      #endregion // Transacted
-
-      #endregion // AlphaFS
-
-      #endregion // WriteAllBytes
+      
 
       #region WriteAllLines
 
@@ -599,7 +517,6 @@ namespace Alphaleonis.Win32.Filesystem
       #endregion // Transacted
 
       #endregion // Compress
-
 
       #region CreateHardlink
 
@@ -1652,10 +1569,6 @@ namespace Alphaleonis.Win32.Filesystem
 
       #endregion GetStreamSize
 
-      
-
-      
-
       #region RemoveStream
 
       #region IsFullPath
@@ -2597,29 +2510,7 @@ namespace Alphaleonis.Win32.Filesystem
 
       #endregion // TransferTimestampsInternal
 
-      #region WriteAllBytesInternal
-
-      /// <summary>
-      ///   [AlphaFS] Unified method WriteAllBytesInternal() to create a new file as part of a transaction, writes the specified byte array to
-      ///   the file, and then closes the file. If the target file already exists, it is overwritten.
-      /// </summary>
-      /// <exception cref="ArgumentNullException">Thrown when one or more required arguments are null.</exception>
-      /// <param name="transaction">The transaction.</param>
-      /// <param name="path">The file to write to.</param>
-      /// <param name="bytes">The bytes to write to the file.</param>
-      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
-      [SuppressMessage("Microsoft.Naming", "CA1720:IdentifiersShouldNotContainTypeNames", MessageId = "bytes")]
-      [SecurityCritical]
-      internal static void WriteAllBytesInternal(KernelTransaction transaction, string path, byte[] bytes, PathFormat pathFormat)
-      {
-         if (bytes == null)
-            throw new ArgumentNullException("bytes");
-
-         using (FileStream fs = OpenInternal(transaction, path, FileMode.Create, 0, FileAccess.Write, FileShare.Read, ExtendedFileAttributes.None, pathFormat))
-            fs.Write(bytes, 0, bytes.Length);
-      }
-
-      #endregion // WriteAllBytesInternal
+      
 
       
 
