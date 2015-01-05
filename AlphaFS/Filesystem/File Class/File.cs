381b1e0e680253cb74edeb5a26253ecde0dbab5f
Peter Palotas <alphaleonis@users.noreply.github.com>
2015-01-05 20:12:49 +0100

    Extracted File.ReadAllLines

--- AlphaFS/Filesystem/File Class/File.cs
+++ AlphaFS/Filesystem/File Class/File.cs
@@ -48,117 +48,7 @@ namespace Alphaleonis.Win32.Filesystem
 
       
 
-      #region ReadAllLines
-
-      #region .NET
-
-      /// <summary>Opens a text file, reads all lines of the file, and then closes the file.</summary>
-      /// <param name="path">The file to open for reading.</param>
-      /// <returns>All lines of the file.</returns>
-      [SecurityCritical]
-      public static string[] ReadAllLines(string path)
-      {
-         return ReadAllLinesInternal(null, path, NativeMethods.DefaultFileEncoding, PathFormat.Auto).ToArray();
-      }
-
-      /// <summary>Opens a file, reads all lines of the file with the specified encoding, and then closes the file.</summary>
-      /// <param name="path">The file to open for reading.</param>
-      /// <param name="encoding">The <see cref="Encoding"/> applied to the contents of the file.</param>
-      /// <returns>All lines of the file.</returns>
-      [SecurityCritical]
-      public static string[] ReadAllLines(string path, Encoding encoding)
-      {
-         return ReadAllLinesInternal(null, path, encoding, PathFormat.Auto).ToArray();
-      }
-
-      #endregion // .NET
-
-      #region AlphaFS
-
-      #region IsFullPath
-
-      /// <summary>[AlphaFS] Opens a text file, reads all lines of the file, and then closes the file.</summary>
-      /// <param name="path">The file to open for reading.</param>
-      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
-      /// <returns>All lines of the file.</returns>
-      [SecurityCritical]
-      public static string[] ReadAllLines(string path, PathFormat pathFormat)
-      {
-         return ReadAllLinesInternal(null, path, NativeMethods.DefaultFileEncoding, pathFormat).ToArray();
-      }
-
-      /// <summary>[AlphaFS] Opens a file, reads all lines of the file with the specified encoding, and then closes the file.</summary>
-      /// <param name="path">The file to open for reading.</param>
-      /// <param name="encoding">The <see cref="Encoding"/> applied to the contents of the file.</param>
-      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
-      /// <returns>All lines of the file.</returns>
-      [SecurityCritical]
-      public static string[] ReadAllLines(string path, Encoding encoding, PathFormat pathFormat)
-      {
-         return ReadAllLinesInternal(null, path, encoding, pathFormat).ToArray();
-      }
-
-      #endregion // IsFullPath
-
-      #region Transacted
-
-      #region .NET
-
-      /// <summary>[AlphaFS] Opens a text file, reads all lines of the file, and then closes the file.</summary>
-      /// <param name="transaction">The transaction.</param>
-      /// <param name="path">The file to open for reading.</param>
-      /// <returns>All lines of the file.</returns>
-      [SecurityCritical]
-      public static string[] ReadAllLines(KernelTransaction transaction, string path)
-      {
-         return ReadAllLinesInternal(transaction, path, NativeMethods.DefaultFileEncoding, PathFormat.Auto).ToArray();
-      }
-
-      /// <summary>[AlphaFS] Opens a file, reads all lines of the file with the specified encoding, and then closes the file.</summary>
-      /// <param name="transaction">The transaction.</param>
-      /// <param name="path">The file to open for reading.</param>
-      /// <param name="encoding">The <see cref="Encoding"/> applied to the contents of the file.</param>
-      /// <returns>All lines of the file.</returns>
-      [SecurityCritical]
-      public static string[] ReadAllLines(KernelTransaction transaction, string path, Encoding encoding)
-      {
-         return ReadAllLinesInternal(transaction, path, encoding, PathFormat.Auto).ToArray();
-      }
-
-      #endregion // .NET
-
-      #region IsFullPath
-
-      /// <summary>[AlphaFS] Opens a text file, reads all lines of the file, and then closes the file.</summary>
-      /// <param name="transaction">The transaction.</param>
-      /// <param name="path">The file to open for reading.</param>
-      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
-      /// <returns>All lines of the file.</returns>
-      [SecurityCritical]
-      public static string[] ReadAllLines(KernelTransaction transaction, string path, PathFormat pathFormat)
-      {
-         return ReadAllLinesInternal(transaction, path, NativeMethods.DefaultFileEncoding, pathFormat).ToArray();
-      }
-
-      /// <summary>[AlphaFS] Opens a file, reads all lines of the file with the specified encoding, and then closes the file.</summary>
-      /// <param name="transaction">The transaction.</param>
-      /// <param name="path">The file to open for reading.</param>
-      /// <param name="encoding">The <see cref="Encoding"/> applied to the contents of the file.</param>
-      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
-      /// <returns>All lines of the file.</returns>
-      [SecurityCritical]
-      public static string[] ReadAllLines(KernelTransaction transaction, string path, Encoding encoding, PathFormat pathFormat)
-      {
-         return ReadAllLinesInternal(transaction, path, encoding, pathFormat).ToArray();
-      }
-
-      #endregion // IsFullPath
-
-      #endregion // Transacted
-
-      #endregion // AlphaFS
-
-      #endregion // ReadAllLines
+      
 
       #region ReadAllText
 
@@ -3693,30 +3583,7 @@ namespace Alphaleonis.Win32.Filesystem
 
       
 
-      #region ReadAllLinesInternal
-
-      /// <summary>
-      ///   [AlphaFS] Unified method ReadAllLinesInternal() to open a file, read all lines of the file with the specified encoding, and then
-      ///   close the file.
-      /// </summary>
-      /// <param name="transaction">The transaction.</param>
-      /// <param name="path">The file to open for reading.</param>
-      /// <param name="encoding">The <see cref="Encoding"/> applied to the contents of the file.</param>
-      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
-      /// <returns>An IEnumerable string containing all lines of the file.</returns>
-      [SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
-      [SecurityCritical]
-      internal static IEnumerable<string> ReadAllLinesInternal(KernelTransaction transaction, string path, Encoding encoding, PathFormat pathFormat)
-      {
-         using (StreamReader sr = new StreamReader(OpenInternal(transaction, path, FileMode.Open, 0, FileAccess.Read, FileShare.Read, ExtendedFileAttributes.SequentialScan, pathFormat), encoding))
-         {
-            string line;
-            while ((line = sr.ReadLine()) != null)
-               yield return line;
-         }
-      }
-
-      #endregion // ReadAllLinesInternal
+      
 
       #region ReadAllTextInternal
 
