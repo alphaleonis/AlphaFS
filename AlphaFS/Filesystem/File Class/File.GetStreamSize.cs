b75e04e0459bfe22733922c236a651cd4008ffc4
Peter Palotas <alphaleonis@users.noreply.github.com>
2015-01-05 20:59:35 +0100

    Extracted File.GetStreamSize methods

---
+++ AlphaFS/Filesystem/File Class/File.GetStreamSize.cs
@@ -0,0 +1,177 @@
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+
+namespace Alphaleonis.Win32.Filesystem
+{
+   public static partial class File
+   {
+      #region Non-Transactional
+
+      /// <summary>[AlphaFS] Retrieves the actual number of bytes of disk storage used by all data streams (NTFS ADS).</summary>
+      /// <param name="path">The path to an existing file.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      /// <returns>The number of bytes used by all data streams.</returns>      
+      [SecurityCritical]
+      public static long GetStreamSize(string path, PathFormat pathFormat)
+      {
+         return AlternateDataStreamInfo.GetStreamSizeInternal(false, null, null, path, null, null, pathFormat);
+      }
+
+      /// <summary>[AlphaFS] Retrieves the actual number of bytes of disk storage used by a named data streams (NTFS ADS).</summary>
+      /// <param name="path">The path to an existing file.</param>
+      /// <param name="name">The name of the stream to retrieve.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      /// <returns>The number of bytes used by a named stream.</returns>      
+      [SecurityCritical]
+      public static long GetStreamSize(string path, string name, PathFormat pathFormat)
+      {
+         return AlternateDataStreamInfo.GetStreamSizeInternal(false, null, null, path, name, StreamType.Data, pathFormat);
+      }
+
+      /// <summary>
+      ///   [AlphaFS] Retrieves the actual number of bytes of disk storage used by a <see cref="StreamType"/> data streams (NTFS ADS).
+      /// </summary>
+      /// <param name="path">The path to an existing file.</param>
+      /// <param name="type">The <see cref="StreamType"/> of the stream to retrieve.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      /// <returns>The number of bytes used by stream of type <see cref="StreamType"/>.</returns>      
+      [SecurityCritical]
+      public static long GetStreamSize(string path, StreamType type, PathFormat pathFormat)
+      {
+         return AlternateDataStreamInfo.GetStreamSizeInternal(false, null, null, path, null, type, pathFormat);
+      }
+
+
+      /// <summary>[AlphaFS] Retrieves the actual number of bytes of disk storage used by all data streams (NTFS ADS).</summary>
+      /// <param name="path">The path to an existing file.</param>
+      /// <returns>The number of bytes used by all data streams.</returns>      
+      [SecurityCritical]
+      public static long GetStreamSize(string path)
+      {
+         return AlternateDataStreamInfo.GetStreamSizeInternal(false, null, null, path, null, null, PathFormat.Auto);
+      }
+
+      /// <summary>[AlphaFS] Retrieves the actual number of bytes of disk storage used by a named data streams (NTFS ADS).</summary>
+      /// <param name="path">The path to an existing file.</param>
+      /// <param name="name">The name of the stream to retrieve.</param>
+      /// <returns>The number of bytes used by a named stream.</returns>      
+      [SecurityCritical]
+      public static long GetStreamSize(string path, string name)
+      {
+         return AlternateDataStreamInfo.GetStreamSizeInternal(false, null, null, path, name, StreamType.Data, PathFormat.Auto);
+      }
+
+      /// <summary>
+      ///   [AlphaFS] Retrieves the actual number of bytes of disk storage used by a <see cref="StreamType"/> data streams (NTFS ADS).
+      /// </summary>
+      /// <param name="path">The path to an existing file.</param>
+      /// <param name="type">The <see cref="StreamType"/> of the stream to retrieve.</param>
+      /// <returns>The number of bytes used by stream of type <see cref="StreamType"/>.</returns>      
+      [SecurityCritical]
+      public static long GetStreamSize(string path, StreamType type)
+      {
+         return AlternateDataStreamInfo.GetStreamSizeInternal(false, null, null, path, null, type, PathFormat.Auto);
+      }
+
+      /// <summary>[AlphaFS] Retrieves the actual number of bytes of disk storage used by all data streams (NTFS ADS).</summary>
+      /// <param name="handle">The <see cref="SafeFileHandle"/> to the file.</param>
+      /// <param name="name">The name of the stream to retrieve.</param>
+      /// <returns>The number of bytes used by a named stream.</returns>      
+      [SecurityCritical]
+      public static long GetStreamSize(SafeFileHandle handle, string name)
+      {
+         return AlternateDataStreamInfo.GetStreamSizeInternal(false, null, handle, null, name, StreamType.Data, PathFormat.ExtendedLength);
+      }
+
+      /// <summary>
+      ///   [AlphaFS] Retrieves the actual number of bytes of disk storage used by a <see cref="StreamType"/> data streams (NTFS ADS).
+      /// </summary>
+      /// <param name="handle">The <see cref="SafeFileHandle"/> to the file.</param>
+      /// <param name="type">The <see cref="StreamType"/> of the stream to retrieve.</param>
+      /// <returns>The number of bytes used by stream of type <see cref="StreamType"/>.</returns>      
+      [SecurityCritical]
+      public static long GetStreamSize(SafeFileHandle handle, StreamType type)
+      {
+         return AlternateDataStreamInfo.GetStreamSizeInternal(false, null, handle, null, null, type, PathFormat.ExtendedLength);
+      }
+
+      #endregion
+
+      #region Transactional
+
+      /// <summary>[AlphaFS] Retrieves the actual number of bytes of disk storage used by all data streams (NTFS ADS).</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The path to an existing file.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      /// <returns>The number of bytes used by all data streams.</returns>      
+      [SecurityCritical]
+      public static long GetStreamSize(KernelTransaction transaction, string path, PathFormat pathFormat)
+      {
+         return AlternateDataStreamInfo.GetStreamSizeInternal(false, transaction, null, path, null, null, pathFormat);
+      }
+
+      /// <summary>[AlphaFS] Retrieves the actual number of bytes of disk storage used by a named data streams (NTFS ADS).</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The path to an existing file.</param>
+      /// <param name="name">The name of the stream to retrieve.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      /// <returns>The number of bytes used by a named stream.</returns>      
+      [SecurityCritical]
+      public static long GetStreamSize(KernelTransaction transaction, string path, string name, PathFormat pathFormat)
+      {
+         return AlternateDataStreamInfo.GetStreamSizeInternal(false, transaction, null, path, name, StreamType.Data, pathFormat);
+      }
+
+      /// <summary>
+      ///   [AlphaFS] Retrieves the actual number of bytes of disk storage used by a <see cref="StreamType"/> data streams (NTFS ADS).
+      /// </summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The path to an existing file.</param>
+      /// <param name="type">The <see cref="StreamType"/> of the stream to retrieve.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      /// <returns>The number of bytes used by stream of type <see cref="StreamType"/>.</returns>      
+      [SecurityCritical]
+      public static long GetStreamSize(KernelTransaction transaction, string path, StreamType type, PathFormat pathFormat)
+      {
+         return AlternateDataStreamInfo.GetStreamSizeInternal(false, transaction, null, path, null, type, pathFormat);
+      }
+
+
+      /// <summary>[AlphaFS] Retrieves the actual number of bytes of disk storage used by all data streams (NTFS ADS).</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The path to an existing file.</param>
+      /// <returns>The number of bytes used by all data streams.</returns>      
+      [SecurityCritical]
+      public static long GetStreamSize(KernelTransaction transaction, string path)
+      {
+         return AlternateDataStreamInfo.GetStreamSizeInternal(false, transaction, null, path, null, null, PathFormat.Auto);
+      }
+
+      /// <summary>[AlphaFS] Retrieves the actual number of bytes of disk storage used by a named data streams (NTFS ADS).</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The path to an existing file.</param>
+      /// <param name="name">The name of the stream to retrieve.</param>
+      /// <returns>The number of bytes used by a named stream.</returns>      
+      [SecurityCritical]
+      public static long GetStreamSize(KernelTransaction transaction, string path, string name)
+      {
+         return AlternateDataStreamInfo.GetStreamSizeInternal(false, transaction, null, path, name, null, PathFormat.Auto);
+      }
+
+      /// <summary>[AlphaFS] Retrieves the actual number of bytes of disk storage used by all data streams (NTFS ADS).</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The path to an existing file.</param>
+      /// <param name="type">The <see cref="StreamType"/> of the stream to retrieve.</param>
+      /// <returns>The number of bytes used by stream of type <see cref="StreamType"/>.</returns>      
+      [SecurityCritical]
+      public static long GetStreamSize(KernelTransaction transaction, string path, StreamType type)
+      {
+         return AlternateDataStreamInfo.GetStreamSizeInternal(false, transaction, null, path, null, type, PathFormat.Auto);
+      }
+
+      #endregion // Transacted
+   }
+}
