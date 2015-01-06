6867155bd8afb5c5522c5607ca6f3aba8e0cb3a4
Peter Palotas <alphaleonis@users.noreply.github.com>
2015-01-06 20:16:23 +0100

    Extracted Directory.SetXXXTime methods.

--- AlphaFS/Filesystem/Directory Class/Directory.CopyMove.cs
+++ AlphaFS/Filesystem/Directory Class/Directory.CopyMove.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Security;
+using System.IO;
 
 namespace Alphaleonis.Win32.Filesystem
 {
---
+++ AlphaFS/Filesystem/Directory Class/Directory.SetTimes.cs
@@ -0,0 +1,273 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+
+namespace Alphaleonis.Win32.Filesystem
+{
+   public static partial class Directory
+   {
+      #region SetCreationTime
+
+      /// <summary>Sets the date and time the directory was created.</summary>
+      /// <param name="path">The directory for which to set the creation date and time information.</param>
+      /// <param name="creationTime">A <see cref="System.DateTime"/> containing the value to set for the creation date and time of <paramref name="path"/>. This value is expressed in local time.</param>
+      [SecurityCritical]
+      public static void SetCreationTime(string path, DateTime creationTime)
+      {
+         File.SetFsoDateTimeInternal(true, null, path, creationTime.ToUniversalTime(), null, null, PathFormat.Relative);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time the directory was created.</summary>
+      /// <param name="path">The directory for which to set the creation date and time information.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      /// <param name="creationTime">A <see cref="System.DateTime"/> containing the value to set for the creation date and time of <paramref name="path"/>. This value is expressed in local time.</param>
+      [SecurityCritical]
+      public static void SetCreationTime(string path, DateTime creationTime, PathFormat pathFormat)
+      {
+         File.SetFsoDateTimeInternal(true, null, path, creationTime.ToUniversalTime(), null, null, pathFormat);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time the directory was created.</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The directory for which to set the creation date and time information.</param>
+      /// <param name="creationTime">A <see cref="System.DateTime"/> containing the value to set for the creation date and time of <paramref name="path"/>. This value is expressed in local time.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      [SecurityCritical]
+      public static void SetCreationTime(KernelTransaction transaction, string path, DateTime creationTime, PathFormat pathFormat)
+      {
+         File.SetFsoDateTimeInternal(true, transaction, path, creationTime.ToUniversalTime(), null, null, pathFormat);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time the directory was created.</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The directory for which to set the creation date and time information.</param>
+      /// <param name="creationTime">A <see cref="System.DateTime"/> containing the value to set for the creation date and time of <paramref name="path"/>. This value is expressed in local time.</param>
+      [SecurityCritical]
+      public static void SetCreationTime(KernelTransaction transaction, string path, DateTime creationTime)
+      {
+         File.SetFsoDateTimeInternal(true, transaction, path, creationTime.ToUniversalTime(), null, null, PathFormat.Relative);
+      }
+
+      #endregion // SetCreationTime
+
+      #region SetCreationTimeUtc
+
+      /// <summary>Sets the date and time, in coordinated universal time (UTC), that the directory was created.</summary>
+      /// <param name="path">The directory for which to set the creation date and time information.</param>
+      /// <param name="creationTimeUtc">A <see cref="System.DateTime"/> containing the value to set for the creation date and time of <paramref name="path"/>. This value is expressed in UTC time.</param>
+      [SecurityCritical]
+      public static void SetCreationTimeUtc(string path, DateTime creationTimeUtc)
+      {
+         File.SetFsoDateTimeInternal(true, null, path, creationTimeUtc, null, null, PathFormat.Relative);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time, in coordinated universal time (UTC), that the directory was created.</summary>
+      /// <param name="path">The directory for which to set the creation date and time information.</param>
+      /// <param name="creationTimeUtc">A <see cref="System.DateTime"/> containing the value to set for the creation date and time of <paramref name="path"/>. This value is expressed in UTC time.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      [SecurityCritical]
+      public static void SetCreationTimeUtc(string path, DateTime creationTimeUtc, PathFormat pathFormat)
+      {
+         File.SetFsoDateTimeInternal(true, null, path, creationTimeUtc, null, null, pathFormat);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time, in coordinated universal time (UTC), that the directory was created.</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The directory for which to set the creation date and time information.</param>
+      /// <param name="creationTimeUtc">A <see cref="System.DateTime"/> containing the value to set for the creation date and time of <paramref name="path"/>. This value is expressed in UTC time.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      [SecurityCritical]
+      public static void SetCreationTimeUtc(KernelTransaction transaction, string path, DateTime creationTimeUtc, PathFormat pathFormat)
+      {
+         File.SetFsoDateTimeInternal(true, transaction, path, creationTimeUtc, null, null, pathFormat);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time, in coordinated universal time (UTC), that the directory was created.</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The directory for which to set the creation date and time information.</param>
+      /// <param name="creationTimeUtc">A <see cref="System.DateTime"/> containing the value to set for the creation date and time of <paramref name="path"/>. This value is expressed in UTC time.</param>
+      [SecurityCritical]
+      public static void SetCreationTimeUtc(KernelTransaction transaction, string path, DateTime creationTimeUtc)
+      {
+         File.SetFsoDateTimeInternal(true, transaction, path, creationTimeUtc, null, null, PathFormat.Relative);
+      }
+
+      #endregion // SetCreationTimeUtc
+
+      #region SetLastAccessTime
+
+      /// <summary>Sets the date and time that the specified directory was last accessed.</summary>
+      /// <param name="path">The file for which to set the access date and time information.</param>
+      /// <param name="lastAccessTime">A <see cref="System.DateTime"/> containing the value to set for the last access date and time of <paramref name="path"/>. This value is expressed in local time.</param>
+      [SecurityCritical]
+      public static void SetLastAccessTime(string path, DateTime lastAccessTime)
+      {
+         File.SetFsoDateTimeInternal(true, null, path, null, lastAccessTime.ToUniversalTime(), null, PathFormat.Relative);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time that the specified directory was last accessed.</summary>
+      /// <param name="path">The file for which to set the access date and time information.</param>
+      /// <param name="lastAccessTime">A <see cref="System.DateTime"/> containing the value to set for the last access date and time of <paramref name="path"/>. This value is expressed in local time.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      [SecurityCritical]
+      public static void SetLastAccessTime(string path, DateTime lastAccessTime, PathFormat pathFormat)
+      {
+         File.SetFsoDateTimeInternal(true, null, path, null, lastAccessTime.ToUniversalTime(), null, pathFormat);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time that the specified directory was last accessed.</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The directory for which to set the access date and time information.</param>
+      /// <param name="lastAccessTime">A <see cref="System.DateTime"/> containing the value to set for the last access date and time of <paramref name="path"/>. This value is expressed in local time.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      [SecurityCritical]
+      public static void SetLastAccessTime(KernelTransaction transaction, string path, DateTime lastAccessTime, PathFormat pathFormat)
+      {
+         File.SetFsoDateTimeInternal(true, transaction, path, null, lastAccessTime.ToUniversalTime(), null, pathFormat);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time that the specified directory was last accessed.</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The directory for which to set the access date and time information.</param>
+      /// <param name="lastAccessTime">A <see cref="System.DateTime"/> containing the value to set for the last access date and time of <paramref name="path"/>. This value is expressed in local time.</param>
+      [SecurityCritical]
+      public static void SetLastAccessTime(KernelTransaction transaction, string path, DateTime lastAccessTime)
+      {
+         File.SetFsoDateTimeInternal(true, transaction, path, null, lastAccessTime.ToUniversalTime(), null, PathFormat.Relative);
+      }
+
+      #endregion // SetLastAccessTime
+
+      #region SetLastAccessTimeUtc
+
+      /// <summary>Sets the date and time, in coordinated universal time (UTC), that the specified directory was last accessed.</summary>
+      /// <param name="path">The directory for which to set the access date and time information.</param>
+      /// <param name="lastAccessTimeUtc">A <see cref="System.DateTime"/> containing the value to set for the last access date and time of <paramref name="path"/>. This value is expressed in UTC time.</param>
+      [SecurityCritical]
+      public static void SetLastAccessTimeUtc(string path, DateTime lastAccessTimeUtc)
+      {
+         File.SetFsoDateTimeInternal(true, null, path, null, lastAccessTimeUtc, null, PathFormat.Relative);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time, in coordinated universal time (UTC), that the specified directory was last accessed.</summary>
+      /// <param name="path">The directory for which to set the access date and time information.</param>
+      /// <param name="lastAccessTimeUtc">A <see cref="System.DateTime"/> containing the value to set for the last access date and time of <paramref name="path"/>. This value is expressed in UTC time.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      [SecurityCritical]
+      public static void SetLastAccessTimeUtc(string path, DateTime lastAccessTimeUtc, PathFormat pathFormat)
+      {
+         File.SetFsoDateTimeInternal(true, null, path, null, lastAccessTimeUtc, null, pathFormat);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time, in coordinated universal time (UTC), that the specified directory was last accessed.</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The directory for which to set the access date and time information.</param>
+      /// <param name="lastAccessTimeUtc">A <see cref="System.DateTime"/> containing the value to set for the last access date and time of <paramref name="path"/>. This value is expressed in UTC time.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      [SecurityCritical]
+      public static void SetLastAccessTimeUtc(KernelTransaction transaction, string path, DateTime lastAccessTimeUtc, PathFormat pathFormat)
+      {
+         File.SetFsoDateTimeInternal(true, transaction, path, null, lastAccessTimeUtc, null, pathFormat);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time, in coordinated universal time (UTC), that the specified directory was last accessed.</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The directory for which to set the access date and time information.</param>
+      /// <param name="lastAccessTimeUtc">A <see cref="System.DateTime"/> containing the value to set for the last access date and time of <paramref name="path"/>. This value is expressed in UTC time.</param>
+      [SecurityCritical]
+      public static void SetLastAccessTimeUtc(KernelTransaction transaction, string path, DateTime lastAccessTimeUtc)
+      {
+         File.SetFsoDateTimeInternal(true, transaction, path, null, lastAccessTimeUtc, null, PathFormat.Relative);
+      }
+
+      #endregion // SetLastAccessTimeUtc
+
+      #region SetLastWriteTime
+
+      /// <summary>Sets the date and time that the specified directory was last written to.</summary>
+      /// <param name="path">The directory for which to set the date and time information.</param>
+      /// <param name="lastWriteTime">A <see cref="System.DateTime"/> containing the value to set for the last write date and time of <paramref name="path"/>. This value is expressed in local time.</param>
+      [SecurityCritical]
+      public static void SetLastWriteTime(string path, DateTime lastWriteTime)
+      {
+         File.SetFsoDateTimeInternal(true, null, path, null, null, lastWriteTime.ToUniversalTime(), PathFormat.Relative);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time that the specified directory was last written to.</summary>
+      /// <param name="path">The directory for which to set the date and time information.</param>
+      /// <param name="lastWriteTime">A <see cref="System.DateTime"/> containing the value to set for the last write date and time of <paramref name="path"/>. This value is expressed in local time.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      [SecurityCritical]
+      public static void SetLastWriteTime(string path, DateTime lastWriteTime, PathFormat pathFormat)
+      {
+         File.SetFsoDateTimeInternal(true, null, path, null, null, lastWriteTime.ToUniversalTime(), pathFormat);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time that the specified directory was last written to.</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The directory for which to set the date and time information.</param>
+      /// <param name="lastWriteTime">A <see cref="System.DateTime"/> containing the value to set for the last write date and time of <paramref name="path"/>. This value is expressed in local time.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      [SecurityCritical]
+      public static void SetLastWriteTime(KernelTransaction transaction, string path, DateTime lastWriteTime, PathFormat pathFormat)
+      {
+         File.SetFsoDateTimeInternal(true, transaction, path, null, null, lastWriteTime.ToUniversalTime(), pathFormat);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time that the specified directory was last written to.</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The directory for which to set the date and time information.</param>
+      /// <param name="lastWriteTime">A <see cref="System.DateTime"/> containing the value to set for the last write date and time of <paramref name="path"/>. This value is expressed in local time.</param>
+      [SecurityCritical]
+      public static void SetLastWriteTime(KernelTransaction transaction, string path, DateTime lastWriteTime)
+      {
+         File.SetFsoDateTimeInternal(true, transaction, path, null, null, lastWriteTime.ToUniversalTime(), PathFormat.Relative);
+      }
+      #endregion // SetLastWriteTime
+
+      #region SetLastWriteTimeUtc
+
+      /// <summary>Sets the date and time, in coordinated universal time (UTC), that the specified directory was last written to.</summary>
+      /// <param name="path">The directory for which to set the date and time information.</param>
+      /// <param name="lastWriteTimeUtc">A <see cref="System.DateTime"/> containing the value to set for the last write date and time of <paramref name="path"/>. This value is expressed in UTC time.</param>
+      [SecurityCritical]
+      public static void SetLastWriteTimeUtc(string path, DateTime lastWriteTimeUtc)
+      {
+         File.SetFsoDateTimeInternal(true, null, path, null, null, lastWriteTimeUtc, PathFormat.Relative);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time, in coordinated universal time (UTC), that the specified directory was last written to.</summary>
+      /// <param name="path">The directory for which to set the date and time information.</param>
+      /// <param name="lastWriteTimeUtc">A <see cref="System.DateTime"/> containing the value to set for the last write date and time of <paramref name="path"/>. This value is expressed in UTC time.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      [SecurityCritical]
+      public static void SetLastWriteTimeUtc(string path, DateTime lastWriteTimeUtc, PathFormat pathFormat)
+      {
+         File.SetFsoDateTimeInternal(true, null, path, null, null, lastWriteTimeUtc, pathFormat);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time, in coordinated universal time (UTC), that the specified directory was last written to.</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The directory for which to set the date and time information.</param>
+      /// <param name="lastWriteTimeUtc">A <see cref="System.DateTime"/> containing the value to set for the last write date and time of <paramref name="path"/>. This value is expressed in UTC time.</param>
+      /// <param name="pathFormat">Indicates the format of the path parameter(s).</param>
+      [SecurityCritical]
+      public static void SetLastWriteTimeUtc(KernelTransaction transaction, string path, DateTime lastWriteTimeUtc, PathFormat pathFormat)
+      {
+         File.SetFsoDateTimeInternal(true, transaction, path, null, null, lastWriteTimeUtc, pathFormat);
+      }
+
+      /// <summary>[AlphaFS] Sets the date and time, in coordinated universal time (UTC), that the specified directory was last written to.</summary>
+      /// <param name="transaction">The transaction.</param>
+      /// <param name="path">The directory for which to set the date and time information.</param>
+      /// <param name="lastWriteTimeUtc">A <see cref="System.DateTime"/> containing the value to set for the last write date and time of <paramref name="path"/>. This value is expressed in UTC time.</param>
+      [SecurityCritical]
+      public static void SetLastWriteTimeUtc(KernelTransaction transaction, string path, DateTime lastWriteTimeUtc)
+      {
+         File.SetFsoDateTimeInternal(true, transaction, path, null, null, lastWriteTimeUtc, PathFormat.Relative);
+      }
+
+      #endregion // SetLastWriteTimeUtc
+   }
+}
