4503760cb19aeef0801943a21add30a6a038f76b
yomodo <shogun.net@outlook.com>
2015-01-08 18:59:05 +0100

    Fixes #27

---
+++ AlphaFS/Filesystem/FileInfo Class/FileInfo.AddAlternateDataStream.cs
@@ -0,0 +1,41 @@
+﻿/* Copyright (C) 2008-2015 Peter Palotas, Jeffrey Jangli, Alexandr Normuradov
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy 
+ *  of this software and associated documentation files (the "Software"), to deal 
+ *  in the Software without restriction, including without limitation the rights 
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ *  copies of the Software, and to permit persons to whom the Software is 
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in 
+ *  all copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+ *  THE SOFTWARE. 
+ */
+
+using System.Security;
+
+namespace Alphaleonis.Win32.Filesystem
+{
+   partial class FileInfo
+   {
+      #region AlphaFS
+
+      /// <summary>[AlphaFS] Adds an alternate data stream (NTFS ADS) to the file.</summary>
+      /// <param name="name">The name for the stream. If a stream with <paramref name="name"/> already exists, it will be overwritten.</param>
+      /// <param name="contents">The lines to add to the stream.</param>      
+      [SecurityCritical]
+      public void AddAlternateDataStream(string name, string[] contents)
+      {
+         AlternateDataStreamInfo.AddAlternateDataStreamInternal(false, Transaction, LongFullName, name, contents, PathFormat.LongFullPath);
+      }
+
+      #endregion // AlphaFS
+   }
+}
---
+++ AlphaFS/Filesystem/FileInfo Class/FileInfo.Delete.cs
@@ -0,0 +1,53 @@
+﻿/* Copyright (C) 2008-2015 Peter Palotas, Jeffrey Jangli, Alexandr Normuradov
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy 
+ *  of this software and associated documentation files (the "Software"), to deal 
+ *  in the Software without restriction, including without limitation the rights 
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ *  copies of the Software, and to permit persons to whom the Software is 
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in 
+ *  all copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+ *  THE SOFTWARE. 
+ */
+
+using System.IO;
+
+namespace Alphaleonis.Win32.Filesystem
+{
+   partial class FileInfo
+   {
+      #region .NET
+
+      /// <summary>Permanently deletes a file.</summary>
+      /// <remarks>If the file does not exist, this method does nothing.</remarks>
+      ///
+      /// <exception cref="IOException">.</exception>
+      public override void Delete()
+      {
+         File.DeleteFileInternal(Transaction, LongFullName, false, PathFormat.LongFullPath);
+      }
+
+      #endregion // .NET
+
+      #region AlphaFS
+
+      /// <summary>[AlphaFS] Permanently deletes a file.</summary>
+      /// <remarks>If the file does not exist, this method does nothing.</remarks>
+      /// <param name="ignoreReadOnly"><see langword="true"/> overrides the read only <see cref="FileAttributes"/> of the file.</param>      
+      public void Delete(bool ignoreReadOnly)
+      {
+         File.DeleteFileInternal(Transaction, LongFullName, ignoreReadOnly, PathFormat.LongFullPath);
+      }
+
+      #endregion // AlphaFS
+   }
+}
