f2cedceddea65d0a08b912e928966a2fb90fdbbe
yomodo <shogun.net@outlook.com>
2015-01-10 14:32:44 +0100

    WIP1

--- AlphaFS/Filesystem/Enumerations/GetFullPathOptions.cs
+++ AlphaFS/Filesystem/Enumerations/GetFullPathOptions.cs
@@ -18,18 +18,7 @@
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
  *  THE SOFTWARE. 
  */
-using Alphaleonis.Win32.Network;
-using Alphaleonis.Win32.Security;
-using Microsoft.Win32.SafeHandles;
 using System;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security;
-using System.Text;
 
 namespace Alphaleonis.Win32.Filesystem
 {
@@ -37,14 +26,10 @@ namespace Alphaleonis.Win32.Filesystem
    [Flags]
    internal enum GetFullPathOptions
    {
-      /// <summary>
-      /// No special options applies.
-      /// </summary>
+      /// <summary>No special options applies.</summary>
       None = 0,
 
-      /// <summary>
-      /// Remove any trailing whitespace from the path.
-      /// </summary>
+      /// <summary>Remove any trailing whitespace from the path.</summary>
       TrimEnd = 1,
 
       /// <summary>
@@ -52,24 +37,21 @@ namespace Alphaleonis.Win32.Filesystem
       /// </summary>
       AddTrailingDirectorySeparator = 2,
 
-      /// <summary>
-      /// Remove the trailing directory separator from the path (if one exists).
-      /// </summary>
+      /// <summary>Remove the trailing directory separator from the path (if one exists).</summary>
       RemoveTrailingDirectorySeparator = 4,
 
-      /// <summary>
-      /// Prevents and exception from being thrown if a filesystem object does not exist.
-      /// </summary>
+      /// <summary>Prevents and exception from being thrown if a filesystem object does not exist.</summary>
       ContinueOnNonExist = 8,
 
-      /// <summary>
-      /// Check that the path contains only valid path-characters.
-      /// </summary>
+      /// <summary>Check that the path contains only valid path-characters.</summary>
       CheckInvalidPathChars = 16,
 
-      /// <summary>
-      /// Also check for wildcard (? and *) characters.
-      /// </summary>
+      /// <summary>Also check for wildcard (? and *) characters.</summary>
       CheckAdditional = 32,
+
+      /// <summary>
+      /// 
+      /// </summary>
+      SkipChangeErrorMode = 64
    }
 }
