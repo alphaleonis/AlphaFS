a22c5aa2bd1524c5f7e2e86e4a75d640a49eedff
yomodo <shogun.net@outlook.com>
2015-01-13 16:56:16 +0100

    -Updated comments of enum ReparsePointTag.

--- AlphaFS/Filesystem/Enumerations/ReparsePointTag.cs
+++ AlphaFS/Filesystem/Enumerations/ReparsePointTag.cs
@@ -23,67 +23,84 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Alphaleonis.Win32.Filesystem
 {
-   /// <summary>
-   ///   Enumeration specifying the different reparse point tags.
-   /// </summary>
+   /// <summary>Enumeration specifying the different reparse point tags.</summary>
    /// <remarks>
-   ///   <para>Reparse tags, with the exception of IO_REPARSE_TAG_SYMLINK, are processed on the server</para>
-   ///   <para>and are not processed by a client after transmission over the wire.</para>
+   ///   <para>Reparse tags, with the exception of IO_REPARSE_TAG_SYMLINK, are processed on the server and are not processed by a client after transmission over the wire.</para>
    ///   <para>Clients should treat associated reparse data as opaque data.</para>
    /// </remarks>
    [SuppressMessage("Microsoft.Design", "CA1028:EnumStorageShouldBeInt32")]
    public enum ReparsePointTag : uint
    {
-      /// <summary>(0x00000000) The entry is not a reparse point.</summary>
+      /// <summary>The entry is not a reparse point.</summary>
       None = 0,
 
-      /// <summary>(0xC0000014) IO_REPARSE_APPXSTREAM</summary>
+      /// <summary>IO_REPARSE_APPXSTREAM</summary>
       AppXStream = 3221225492,
 
-      /// <summary>(0x80000009) IO_REPARSE_TAG_CSV</summary>
+      /// <summary>IO_REPARSE_TAG_CSV</summary>
       Csv = 2147483657,
 
-      /// <summary>(0x80000005) IO_REPARSE_TAG_DRIVER_EXTENDER - Used by Home server drive extender.</summary>
+      /// <summary>IO_REPARSE_TAG_DRIVER_EXTENDER
+      /// <para>Used by Home server drive extender.</para>
+      /// </summary>
       DriverExtender = 2147483653,
 
-      /// <summary>(0x80000013) IO_REPARSE_TAG_DEDUP</summary>
+      /// <summary>IO_REPARSE_TAG_DEDUP</summary>
       [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Dedup")]
       Dedup = 2147483667,
 
-      /// <summary>(0x8000000A) IO_REPARSE_TAG_DFS - Used by the DFS filter.</summary>
+      /// <summary>IO_REPARSE_TAG_DFS
+      /// <para>Used by the DFS filter.</para>
+      /// </summary>
       [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Dfs")]
       Dfs = 2147483658,
 
-      /// <summary>(0x80000012) IO_REPARSE_TAG_DFSR - Used by the DFS filter.</summary>
+      /// <summary>IO_REPARSE_TAG_DFSR
+      /// <para>Used by the DFS filter.</para>
+      /// </summary>
       [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Dfsr")]
       Dfsr = 2147483666,
 
-      /// <summary>(0x8000000B) IO_REPARSE_TAG_FILTER_MANAGER - Used by filter manager test harness.</summary>
+      /// <summary>IO_REPARSE_TAG_FILTER_MANAGER
+      /// <para>Used by filter manager test harness.</para>
+      /// </summary>
       FilterManager = 2147483659,
 
-      /// <summary>(0xC0000004) IO_REPARSE_TAG_HSM - Obsolete. Used by legacy Hierarchical Storage Manager Product.</summary>
+      /// <summary>IO_REPARSE_TAG_HSM
+      /// <para>(Obsolete) Used by legacy Hierarchical Storage Manager Product.</para>
+      /// </summary>
       [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Hsm")]
       Hsm = 3221225476,
 
-      /// <summary>(0x80000006) IO_REPARSE_TAG_HSM2 - Obsolete. Used by legacy Hierarchical Storage Manager Product.</summary>
+      /// <summary>IO_REPARSE_TAG_HSM2
+      /// <para>(Obsolete) Used by legacy Hierarchical Storage Manager Product.</para>
+      /// </summary>
       [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Hsm")]
       Hsm2 = 2147483654,
 
-      /// <summary>(0x80000014) IO_REPARSE_TAG_NFS - NFS symlinks, Windows 8 / SMB3 and later.</summary>
+      /// <summary>IO_REPARSE_TAG_NFS
+      /// <para>NFS symlinks, Windows 8 / SMB3 and later.</para>
+      /// </summary>
       [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Nfs")]
       Nfs = 2147483668,
       
-      /// <summary>(0xA0000003) IO_REPARSE_TAG_MOUNT_POINT - Used for mount point support.</summary>
+      /// <summary>IO_REPARSE_TAG_MOUNT_POINT
+      /// <para>Used for mount point support.</para>
+      /// </summary>
       MountPoint = 2684354563,
 
-      /// <summary>(0x80000007) IO_REPARSE_TAG_SIS - Used by single-instance storage (SIS) filter driver.</summary>
+      /// <summary>IO_REPARSE_TAG_SIS
+      /// <para>Used by single-instance storage (SIS) filter driver.</para>
+      /// </summary>
       Sis = 2147483655,
 
-      /// <summary>(0xA000000C) IO_REPARSE_TAG_SYMLINK - Used for symbolic link support.</summary>
+      /// <summary>IO_REPARSE_TAG_SYMLINK
+      /// <para>Used for symbolic link support.</para>
+      /// </summary>
       [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Sym")]
       SymLink = 2684354572,
 
-      /// <summary>(0x80000008) IO_REPARSE_TAG_WIM</summary>
+      /// <summary>IO_REPARSE_TAG_WIM</summary>
       [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Wim")]
       Wim = 2147483656
    }
