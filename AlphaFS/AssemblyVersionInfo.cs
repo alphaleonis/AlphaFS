29c61eb580ffa0bbe26fa653859889e30f836fca
Alphaleonis <alphaleonis@users.noreply.github.com>
2016-10-30 17:16:32 +0100

    Rebranded 2.1.1 to 2.1.2 because an incorrect package was uploaded to NuGet already.

--- AlphaFS/AssemblyVersionInfo.cs
+++ AlphaFS/AssemblyVersionInfo.cs
@@ -27,7 +27,7 @@ using System.Resources;
 [assembly: AssemblyTrademark("")]
 
 [assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: AssemblyFileVersion("2.1.2.0")]
 [assembly: AssemblyInformationalVersion("2.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
