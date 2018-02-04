e73059f4915c69e165af00fa411cdca825f08389
Jeffrey Jangli <shogun.net@outlook.com>
2018-02-04 19:05:58 +0100

    Updated PowerShell script to demonstrate DirectoryEnumerationFilters.

--- PowerShell/Enumerate-FileSystemEntryInfos.ps1
+++ PowerShell/Enumerate-FileSystemEntryInfos.ps1
@@ -18,12 +18,14 @@
  #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  #  THE SOFTWARE.
  #>
-
-
-Param(
+ 
+ 
+ [CmdletBinding()]
+ Param(
     [String]$Path = '.',
     [String]$Filter = '*',
     [Switch]$Recurse,
+    [Switch]$ShowProgress,
     [Switch]$ContinueOnException,
     [Switch]$Directory,
     [Switch]$File
@@ -37,7 +39,6 @@ Function Invoke-GenericMethod {
         Function to call a C# method with generic parameters.
 #>
 
-    [OutputType([Void])]
     Param(
         [Object]$Instance,
         [String]$MethodName,
@@ -45,29 +46,26 @@ Function Invoke-GenericMethod {
         [Object[]]$MethodParameters
     )
 
+    
     [Collections.ArrayList]$Private:parameterTypes = @{}
+
     ForEach ($Private:paramType In $MethodParameters) { [Void]$parameterTypes.Add($paramType.GetType()) }
 
     $Private:method = $Instance.GetMethod($methodName, "Instance, Static, Public", $Null, $parameterTypes, $Null)
 
-    If ($Null -eq $method) { Throw ('Method: [{0}] not found.' -f ($Instance.ToString() + '.' + $methodName)) }
-    Else {
-        $method = $method.MakeGenericMethod($TypeParameters)
-        $method.Invoke($Instance, $MethodParameters)
-    }
+    If ($Null -eq $method) { Throw ('Method not found: {0}.{1}' -f $Instance.ToString(), $methodName) }
+
+    $method = $method.MakeGenericMethod($TypeParameters)
+
+    $method.Invoke($Instance, $MethodParameters)
 }
 
 
-[ScriptBlock]$ReportException = {
+[ScriptBlock]$ErrorFilter = {
 
 <#
     .SYNOPSIS
-        The callback function that is executed for each Exception
-        that is thrown, while enumerating file system entries.
-
-    .NOTE
-        It seems that the callback is not called
-        when this script is run from Windows PowerShell ISE.
+        Filter to process Exception handling.
 #>
 
     [OutputType([Bool])]
@@ -76,109 +74,246 @@ Function Invoke-GenericMethod {
         [String]$errorMessage,
         [String]$pathProcessed
     )
+    
+    
+    Process {
+        [Int]$Private:ERROR_ACCESS_DENIED = 5
+        [Bool]$Private:gotException = $False
+
+        
+        $gotException = $errorCode -eq $ERROR_ACCESS_DENIED
+
+        Write-Warning -Message ('[{0:000}] Error: ({1}) "{2}"  Path: [{3}]' -f ++$Script:ErrorCount, $errorCode, $errorMessage, $pathProcessed)
 
 
-    [Int]$Private:ERROR_ACCESS_DENIED = 5
+        If ($MaxErrorCount -eq $ErrorCount) {
+            Write-Warning -Message 'Maximum Error count reached. Aborting enumeration.'
+            
+            $cancelSource.Cancel()
+            
+            $gotException = $False
+        }
 
 
-    If ($errorCode -eq $ERROR_ACCESS_DENIED) { Write-Warning -Message ('Error: {0}  {1}  Path: [{2}]' -f $errorCode, $errorMessage, $pathProcessed) }
-
-    # Continue enumeration.
-    return $True
+        # When $True continue enumeration.
+        return $gotException
+    }
 }
 
+
+[ScriptBlock]$InclusionFilter = {
+
+<#
+    .SYNOPSIS
+        Filter to in-/exclude file system entries during the enumeration.
+
+    
+    .NOTE
+        The InclusionFilter provides better filter criteria than the -Filter argument.
+#>
+
+    [OutputType([Bool])]
+    Param(
+        [Alphaleonis.Win32.Filesystem.FileSystemEntryInfo]$fsei
+    )
+
+    
+    Process {
+        ++$Script:FsoCount
+
+        # Example: Only allow files/folders with a certain extension.
+        $Private:findExtensions = ".txt", ".ini", ".log"
+
+        [Bool]$Private:gotMatch = $findExtensions -ccontains $fsei.Extension
+
+        If ($gotMatch) {
+            ++$Script:FoundFsoCount
+
+            # Do any other processing here.
+            If ($fsei.IsDirectory) {
+                # ...
+            }
+
+            Else {
+                # ...
+            }
+        }
+
+        
+        # Write-Progress WILL slow things down CONSIDERABLY; Disable when there is no need for progress report.
+        If ($ShowProgress) { Write-Progress -Activity $fsei.FullPath -Status ('Processed: [{0:N0}] Found: [{1:N0}] Errors: [{2:N0}]' -f $FsoCount, $FoundFsoCount, $ErrorCount) }
+
+        
+        If ($MaxFsoFoundCount -eq $FoundFsoCount) {
+            Write-Warning -Message 'Maximum file system entries found. Aborting enumeration.'
+            
+            $cancelSource.Cancel()
+            
+            $gotMatch = $False
+        }
+
+
+        # When $True the file system entry is returned.
+        Return $gotMatch
+    }
+}
 
 
 Function Enumerate-FileSystemEntryInfos {
 
 <#
     .SYNOPSIS
-        [Alphaleonis.Win32.Filesystem.Directory]::EnumerateFileSystemEntryInfos()
-        AlphaFS 2.1+: A powerful folder/file enumerator which can recover from, and report, access denied exceptions.
+        AlphaFS v2.2 EnumerateFileSystemEntryInfos
 
-
+        A powerful folder/file enumerator which can report and recover from access denied exceptions and supports custom filtering.
+    
+    
     .EXAMPLE
-        PS C:\> .\Enumerate-FileSystemEntryInfos.ps1 -Path $env:windir -Filter *.dll -Recurse
+        PS C:\> .\Enumerate-FileSystemEntryInfos.ps1 -Recurse -Path $ENV:windir
 
+    
+    .NOTES
+        Backup privileges are enables when run elevated.
+        This allows for browsing folders which are normally inaccessible.
 
+    
     .OUTPUTS
-        An [Alphaleonis.Win32.Filesystem.FileSystemEntryInfo] instance:
+        An [Alphaleonis.Win32.Filesystem.FileSystemEntryInfo] instance. For example:
 
         AlternateFileName   = []
-		Attributes          = [Directory]
-		CreationTime        = [22-8-2013 15:36:16]
-		CreationTimeUtc     = [22-8-2013 13:36:16]
-		FileName            = [Windows]
-		FileSize            = [0]
-		FullPath            = [C:\Windows]
-		IsArchive           = [False]
-		IsCompressed        = [False]
-		IsDevice            = [False]
-		IsDirectory         = [True]
-		IsEncrypted         = [False]
-		IsHidden            = [False]
-		IsMountPoint        = [False]
-		IsNormal            = [False]
-		IsNotContentIndexed = [False]
-		IsOffline           = [False]
-		IsReadOnly          = [False]
-		IsReparsePoint      = [False]
+        Attributes          = [Archive]
+        CreationTime        = [22-8-2013 13:00:13]
+        CreationTimeUtc     = [22-8-2013 11:00:13]
+        Extension           = [.exe]
+        FileName            = [notepad.exe]
+        FileSize            = [217600]
+        FullPath            = [C:\Windows\System32\notepad.exe]
+        IsArchive           = [True]
+        IsCompressed        = [False]
+        IsDevice            = [False]
+        IsDirectory         = [False]
+        IsEncrypted         = [False]
+        IsHidden            = [False]
+        IsMountPoint        = [False]
+        IsNormal            = [False]
+        IsNotContentIndexed = [False]
+        IsOffline           = [False]
+        IsReadOnly          = [False]
+        IsReparsePoint      = [False]
         IsSparseFile        = [False]
-		IsSymbolicLink      = [False]
-		IsSystem            = [False]
-		IsTemporary         = [False]
-		LastAccessTime      = [12-1-2018 15:54:39]
-		LastAccessTimeUtc   = [12-1-2018 14:54:39]
-		LastWriteTime       = [12-1-2018 15:54:39]
-		LastWriteTimeUtc    = [12-1-2018 14:54:39]
-		LongFullPath        = [\\?\C:\Windows]
-		ReparsePointTag     = [None]
+        IsSymbolicLink      = [False]
+        IsSystem            = [False]
+        IsTemporary         = [False]
+        LastAccessTime      = [22-8-2013 13:00:13]
+        LastAccessTimeUtc   = [22-8-2013 11:00:13]
+        LastWriteTime       = [22-8-2013 13:00:12]
+        LastWriteTimeUtc    = [22-8-2013 11:00:12]
+        LongFullPath        = [\\?\C:\Windows\System32\notepad.exe]
+        ReparsePointTag     = [None]
 #>
 
-    # SkipReparsePoints = Skip reparse points by default.
-    # LargeCache        = Uses a larger buffer for directory queries, which can increase performance of the find operation.
-    # BasicSearch       = The function does not query the short file name, improving overall enumeration speed.
-
-    $Private:enumOptions = [Alphaleonis.Win32.Filesystem.DirectoryEnumerationOptions]
-    [Alphaleonis.Win32.Filesystem.DirectoryEnumerationOptions]$Private:dirEnumOptions = $enumOptions::SkipReparsePoints -bor $enumOptions::LargeCache -bor $enumOptions::BasicSearch
-
-
-    If ($ContinueOnException.IsPresent) { $dirEnumOptions = $dirEnumOptions -bor $enumOptions::ContinueOnException }
-    If ($Recurse.IsPresent)             { $dirEnumOptions = $dirEnumOptions -bor $enumOptions::Recursive }
-
-    If (-not $Directory.IsPresent -and -not $File.IsPresent) { $dirEnumOptions = $dirEnumOptions -bor $enumOptions::FilesAndFolders }
-
-    If ($Directory.IsPresent) { $dirEnumOptions = $dirEnumOptions -bor $enumOptions::Folders }
-    If ($File.IsPresent)      { $dirEnumOptions = $dirEnumOptions -bor $enumOptions::Files   }
+	[CmdletBinding()]
+	Param(
+		[String]$Path = '.',
+		[String]$Filter = '*',
+		[Switch]$Recurse,
+        [Switch]$ShowProgress,
+		[Switch]$ContinueOnException,
+		[Switch]$Directory,
+		[Switch]$File
+	)
 
 
-    [Alphaleonis.Win32.Filesystem.DirectoryEnumerationFilters]$Private:dirEnumFilters = New-Object -TypeName Alphaleonis.Win32.Filesystem.DirectoryEnumerationFilters
-
-    # The callback [ScriptBlock] to execute.
-    $dirEnumFilters.ErrorFilter = $ReportException
+	Begin {
+        $Error.Clear()
 
 
-    Write-Progress -Activity $Path -Status ('Processing input path: {0}' -f $Path)
+        # Demo counters for the DirectoryEnumerationFilters; Enumeration will abort when either condition is met.
+        [Long]$Script:MaxErrorCount    = 10 #-1
+        [Long]$Script:MaxFsoFoundCount = 10 #-1
+        
+        
+        # SkipReparsePoints = Skip reparse points by default.
+		# LargeCache        = Uses a larger buffer for directory queries, which can increase performance of the find operation.
+		# BasicSearch       = The function does not query the short file name, improving overall enumeration speed.
 
-    ForEach ($Private:fsei In (Invoke-GenericMethod `
-        -Instance           ([Alphaleonis.Win32.Filesystem.Directory]) `
-        -MethodName         EnumerateFileSystemEntryInfos `
-        -TypeParameters     Alphaleonis.Win32.Filesystem.FileSystemEntryInfo `
-        -MethodParameters   $Path, $Filter, $dirEnumOptions, $dirEnumFilters, ([Alphaleonis.Win32.Filesystem.PathFormat]::RelativePath))) {
-
-        Write-Progress -Activity $fsei.FullPath -Status ('Processing input path: {0}' -f $Path)
-
-
-        # Return only the path (as a string).
-        #Write-Output $fsei.FullPath
+		$Private:enumOptions = [Alphaleonis.Win32.Filesystem.DirectoryEnumerationOptions]
+		[Alphaleonis.Win32.Filesystem.DirectoryEnumerationOptions]$Private:dirEnumOptions = $enumOptions::SkipReparsePoints -bor $enumOptions::LargeCache -bor $enumOptions::BasicSearch
 
 
-        # Return FileSystemEntryInfo object.
-        #Write-Output $fsei
+		If ($ContinueOnException.IsPresent) { $dirEnumOptions = $dirEnumOptions -bor $enumOptions::ContinueOnException }
+		If ($Recurse.IsPresent)             { $dirEnumOptions = $dirEnumOptions -bor $enumOptions::Recursive }
+
+		If (-not $Directory.IsPresent -and -not $File.IsPresent) { $dirEnumOptions = $dirEnumOptions -bor $enumOptions::FilesAndFolders }
+
+		If ($Directory.IsPresent) { $dirEnumOptions = $dirEnumOptions -bor $enumOptions::Folders }
+		If ($File.IsPresent)      { $dirEnumOptions = $dirEnumOptions -bor $enumOptions::Files   }
+
+
+		[Alphaleonis.Win32.Filesystem.DirectoryEnumerationFilters]$Private:dirEnumFilters = New-Object -TypeName Alphaleonis.Win32.Filesystem.DirectoryEnumerationFilters
+
+		
+        # Counters to keep track of stuff.
+        [Long]$Script:FsoCount      = 0
+        [Long]$Script:FoundFsoCount = 0
+        [Long]$Script:ErrorCount    = 0
+
+        
+        # Used to abort the enumeration.
+        [System.Threading.CancellationTokenSource]$Script:cancelSource = [System.Threading.CancellationTokenSource]::new()
+        $dirEnumFilters.CancellationToken = $cancelSource.Token
+
+
+        # The callback error handler.
+		$dirEnumFilters.ErrorFilter = $ErrorFilter
+
+        
+        # The callback file/folder handler.
+        $dirEnumFilters.InclusionFilter = $InclusionFilter
+        
+
+        # Enables backup privileges when run elevated; This allows for browsing folders which are normally inaccessible.
+        $Private:privilegeEnabler = New-Object Alphaleonis.Win32.Security.PrivilegeEnabler([Alphaleonis.Win32.Security.Privilege]::Backup)
+
+
+
+
+        [Bool]$ShowProgress = $ShowProgress.IsPresent
+        If ($ShowProgress) { Write-Progress -Activity $Path -Status 'Processing items...' }
+
+
+        $Private:stopwatch = [system.diagnostics.stopwatch]::StartNew()
+	}
+
+
+	Process {
+        Try {
+            Invoke-GenericMethod `
+			    -Instance            ([Alphaleonis.Win32.Filesystem.Directory]) `
+			    -MethodName          EnumerateFileSystemEntryInfos `
+			    -TypeParameters      Alphaleonis.Win32.Filesystem.FileSystemEntryInfo `
+			    -MethodParameters    $Path, $Filter, $dirEnumOptions, $dirEnumFilters, ([Alphaleonis.Win32.Filesystem.PathFormat]::RelativePath)
+        }
+
+        # Place disposing here to ensure it is always executed.
+        Finally {
+            If ($Null -ne $privilegeEnabler) { $privilegeEnabler.Dispose() }
+            If ($Null -ne $cancelSource)     { $cancelSource.Dispose()     }
+        }
+    }
+
+
+    End {
+        [String]$Private:foundFsoText = $(If ($Null -ne $dirEnumFilters.InclusionFilter) { ('Found: [{0}]' -f $FoundFsoCount) } Else { '' })
+
+        [Console]::ForegroundColor = [ConsoleColor]::Green
+        [Console]::WriteLine(('Duration: [{0}]  Processed: [{1:N0}] {2} Errors: [{3:N0}]' -f [Timespan]::FromMilliseconds($stopwatch.Elapsed.TotalMilliseconds), $FsoCount, $foundFsoText, $ErrorCount))
+        [Console]::ResetColor()
     }
 }
 
 
-Import-Module -Name 'PATH TO AlphaFS.dll'
+Import-Module -Name 'D:\AdminJJa\AlphaFS\Bin\Debug\Net452\AlphaFS.dll'
+#Import-Module -Name 'PATH TO AlphaFS.dll'
 
-Enumerate-FileSystemEntryInfos
+Enumerate-FileSystemEntryInfos -Path $Path -Filter $Filter -Recurse:$Recurse.IsPresent -ShowProgress:$ShowProgress.IsPresent -ContinueOnException:$ContinueOnException.IsPresent -Directory:$Directory.IsPresent -File:$File.IsPresent
