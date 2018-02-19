a0d91fd4e885d54a8d20e75d12c1baa29aaf999b
Jeffrey Jangli <shogun.net@outlook.com>
2018-02-19 17:01:08 +0100

    Should fix #417: Using a file opened in append mode will fail after a gc occurs (#418)

--- AlphaFS.UnitTest/File Class/File_Open.cs
+++ AlphaFS.UnitTest/File Class/File_Open.cs
@@ -1,4 +1,4 @@
-﻿/*  Copyright (C) 2008-2017 Peter Palotas, Jeffrey Jangli, Alexandr Normuradov
+/*  Copyright (C) 2008-2017 Peter Palotas, Jeffrey Jangli, Alexandr Normuradov
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy 
  *  of this software and associated documentation files (the "Software"), to deal 
@@ -30,14 +30,6 @@ namespace AlphaFS.UnitTest
       // Pattern: <class>_<function>_<scenario>_<expected result>
 
       [TestMethod]
-      public void File_Open_Create_LocalAndNetwork_Success()
-      {
-         Open_Create(false);
-         Open_Create(true);
-      }
-
-
-      [TestMethod]
       public void File_Open_Append_LocalAndNetwork_Success()
       {
          Open_Append(false);
@@ -45,42 +37,24 @@ namespace AlphaFS.UnitTest
       }
 
 
-
-
-      private void Open_Create(bool isNetwork)
+      [TestMethod]
+      public void File_Open_Append_NoObjectDisposedException_LocalAndNetwork_Success()
       {
-         UnitTestConstants.PrintUnitTestHeader(isNetwork);
-
-         var tempPath = System.IO.Path.GetTempPath();
-         if (isNetwork)
-            tempPath = Alphaleonis.Win32.Filesystem.Path.LocalToUnc(tempPath);
-
-
-         using (var rootDir = new TemporaryDirectory(tempPath, MethodBase.GetCurrentMethod().Name))
-         {
-            var file = rootDir.RandomFileFullPath;
-            Console.WriteLine("\nInput File Path: [{0}]\n", file);
-
-
-            long fileLength;
-            var ten = UnitTestConstants.TenNumbers.Length;
-
-            using (var fs = Alphaleonis.Win32.Filesystem.File.Open(file, System.IO.FileMode.Create))
-            {
-               // According to NotePad++, creates a file type: "ANSI", which is reported as: "Unicode (UTF-8)".
-               fs.Write(UnitTestConstants.StringToByteArray(UnitTestConstants.TenNumbers), 0, ten);
-
-               fileLength = fs.Length;
-            }
-
-            Assert.IsTrue(System.IO.File.Exists(file), "The file does not exists, but is expected to.");
-            Assert.IsTrue(fileLength == ten, "The file is: {0} bytes, but is expected to be: {1} bytes.", fileLength, ten);
-         }
-
-         Console.WriteLine();
+         Open_Append_NoObjectDisposedException(false);
+         Open_Append_NoObjectDisposedException(true);
       }
 
 
+      [TestMethod]
+      public void File_Open_Create_LocalAndNetwork_Success()
+      {
+         Open_Create(false);
+         Open_Create(true);
+      }
+      
+      
+
+      
       private void Open_Append(bool isNetwork)
       {
          UnitTestConstants.PrintUnitTestHeader(isNetwork);
@@ -93,7 +67,7 @@ namespace AlphaFS.UnitTest
          using (var rootDir = new TemporaryDirectory(tempPath, MethodBase.GetCurrentMethod().Name))
          {
             var file = rootDir.RandomFileFullPath;
-            Console.WriteLine("\nInput File Path: [{0}]\n", file);
+            Console.WriteLine("\nInput File Path: [{0}]", file);
 
 
             long fileLength;
@@ -121,6 +95,77 @@ namespace AlphaFS.UnitTest
 
             Assert.IsTrue(System.IO.File.Exists(file), "The file does not exists, but is expected to.");
             Assert.IsTrue(fileLength == 2*ten, "The file is: {0} bytes, but is expected to be: {1} bytes.", fileLength, 2*ten);
+         }
+
+         Console.WriteLine();
+      }
+
+
+      private void Open_Append_NoObjectDisposedException(bool isNetwork)
+      {
+         UnitTestConstants.PrintUnitTestHeader(isNetwork);
+
+         var tempPath = System.IO.Path.GetTempPath();
+         if (isNetwork)
+            tempPath = Alphaleonis.Win32.Filesystem.Path.LocalToUnc(tempPath);
+
+
+         using (var rootDir = new TemporaryDirectory(tempPath, MethodBase.GetCurrentMethod().Name))
+         {
+            var file = rootDir.RandomFileFullPath;
+            Console.WriteLine("\nInput File Path: [{0}]", file);
+
+
+            // Create.
+            using (Alphaleonis.Win32.Filesystem.File.Open(file, System.IO.FileMode.Create)) {}
+
+
+            // Append.
+            using (var fs = Alphaleonis.Win32.Filesystem.File.Open(file, System.IO.FileMode.Append))
+            {
+               var dummyBuffer = new byte[1];
+
+               for (int i = 0; i < 1000; ++i)
+               {
+                  fs.Write(dummyBuffer, 0, dummyBuffer.Length);
+
+                  GC.Collect();  // Force garbage collection, one of the subsequent calls to fs.Write will throw an ObjectDisposedException
+               }
+            }
+         }
+
+         Console.WriteLine();
+      }
+
+
+      private void Open_Create(bool isNetwork)
+      {
+         UnitTestConstants.PrintUnitTestHeader(isNetwork);
+
+         var tempPath = System.IO.Path.GetTempPath();
+         if (isNetwork)
+            tempPath = Alphaleonis.Win32.Filesystem.Path.LocalToUnc(tempPath);
+
+
+         using (var rootDir = new TemporaryDirectory(tempPath, MethodBase.GetCurrentMethod().Name))
+         {
+            var file = rootDir.RandomFileFullPath;
+            Console.WriteLine("\nInput File Path: [{0}]", file);
+
+
+            long fileLength;
+            var ten = UnitTestConstants.TenNumbers.Length;
+
+            using (var fs = Alphaleonis.Win32.Filesystem.File.Open(file, System.IO.FileMode.Create))
+            {
+               // According to NotePad++, creates a file type: "ANSI", which is reported as: "Unicode (UTF-8)".
+               fs.Write(UnitTestConstants.StringToByteArray(UnitTestConstants.TenNumbers), 0, ten);
+
+               fileLength = fs.Length;
+            }
+
+            Assert.IsTrue(System.IO.File.Exists(file), "The file does not exists, but is expected to.");
+            Assert.IsTrue(fileLength == ten, "The file is: {0} bytes, but is expected to be: {1} bytes.", fileLength, ten);
          }
 
          Console.WriteLine();
