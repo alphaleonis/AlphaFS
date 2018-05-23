f16189d335b6ff9b756d44c760423426b4b22690
Jeffrey Jangli <shogun.net@outlook.com>
2018-05-23 14:49:23 +0200

    Split File unit tests.

--- AlphaFS.UnitTest/File Class/File.Open/File_Open_Append.cs
+++ AlphaFS.UnitTest/File Class/File.Open/File_Open_Append.cs
@@ -36,24 +36,6 @@ namespace AlphaFS.UnitTest
          Open_Append(true);
       }
 
-
-      [TestMethod]
-      public void File_Open_Append_NoObjectDisposedException_LocalAndNetwork_Success()
-      {
-         Open_Append_NoObjectDisposedException(false);
-         Open_Append_NoObjectDisposedException(true);
-      }
-
-
-      [TestMethod]
-      public void File_Open_Create_LocalAndNetwork_Success()
-      {
-         Open_Create(false);
-         Open_Create(true);
-      }
-      
-      
-
       
       private void Open_Append(bool isNetwork)
       {
@@ -73,6 +55,8 @@ namespace AlphaFS.UnitTest
             long fileLength;
             var ten = UnitTestConstants.TenNumbers.Length;
 
+
+
             // Create.
             using (var fs = Alphaleonis.Win32.Filesystem.File.Open(file, System.IO.FileMode.Create))
             {
@@ -84,6 +68,8 @@ namespace AlphaFS.UnitTest
             Assert.IsTrue(fileLength == ten, "The file is: {0} bytes, but is expected to be: {1} bytes.", fileLength, ten);
 
 
+            
+            
             // Append.
             using (var fs = Alphaleonis.Win32.Filesystem.File.Open(file, System.IO.FileMode.Append))
             {
@@ -95,77 +81,6 @@ namespace AlphaFS.UnitTest
 
             Assert.IsTrue(System.IO.File.Exists(file), "The file does not exists, but is expected to.");
             Assert.IsTrue(fileLength == 2*ten, "The file is: {0} bytes, but is expected to be: {1} bytes.", fileLength, 2*ten);
-         }
-
-         Console.WriteLine();
-      }
-
-
-      private void Open_Append_NoObjectDisposedException(bool isNetwork)
-      {
-         UnitTestConstants.PrintUnitTestHeader(isNetwork);
-
-         var tempPath = System.IO.Path.GetTempPath();
-         if (isNetwork)
-            tempPath = Alphaleonis.Win32.Filesystem.Path.LocalToUnc(tempPath);
-
-
-         using (var rootDir = new TemporaryDirectory(tempPath, MethodBase.GetCurrentMethod().Name))
-         {
-            var file = rootDir.RandomFileFullPath;
-            Console.WriteLine("\nInput File Path: [{0}]", file);
-
-
-            // Create.
-            using (Alphaleonis.Win32.Filesystem.File.Open(file, System.IO.FileMode.Create)) {}
-
-
-            // Append.
-            using (var fs = Alphaleonis.Win32.Filesystem.File.Open(file, System.IO.FileMode.Append))
-            {
-               var dummyBuffer = new byte[1];
-
-               for (int i = 0; i < 1000; ++i)
-               {
-                  fs.Write(dummyBuffer, 0, dummyBuffer.Length);
-
-                  GC.Collect();  // Force garbage collection, one of the subsequent calls to fs.Write will throw an ObjectDisposedException
-               }
-            }
-         }
-
-         Console.WriteLine();
-      }
-
-
-      private void Open_Create(bool isNetwork)
-      {
-         UnitTestConstants.PrintUnitTestHeader(isNetwork);
-
-         var tempPath = System.IO.Path.GetTempPath();
-         if (isNetwork)
-            tempPath = Alphaleonis.Win32.Filesystem.Path.LocalToUnc(tempPath);
-
-
-         using (var rootDir = new TemporaryDirectory(tempPath, MethodBase.GetCurrentMethod().Name))
-         {
-            var file = rootDir.RandomFileFullPath;
-            Console.WriteLine("\nInput File Path: [{0}]", file);
-
-
-            long fileLength;
-            var ten = UnitTestConstants.TenNumbers.Length;
-
-            using (var fs = Alphaleonis.Win32.Filesystem.File.Open(file, System.IO.FileMode.Create))
-            {
-               // According to NotePad++, creates a file type: "ANSI", which is reported as: "Unicode (UTF-8)".
-               fs.Write(UnitTestConstants.StringToByteArray(UnitTestConstants.TenNumbers), 0, ten);
-
-               fileLength = fs.Length;
-            }
-
-            Assert.IsTrue(System.IO.File.Exists(file), "The file does not exists, but is expected to.");
-            Assert.IsTrue(fileLength == ten, "The file is: {0} bytes, but is expected to be: {1} bytes.", fileLength, ten);
          }
 
          Console.WriteLine();
