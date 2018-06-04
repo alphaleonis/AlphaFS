65d8f1bd29d207b2b54e42eda6734d6a4f9a2f8d
Jeffrey Jangli <shogun.net@outlook.com>
2018-06-04 18:52:00 +0200

    Fixed some warnings.

--- AlphaFS/Filesystem/File Class/File.Copy.cs
+++ AlphaFS/Filesystem/File Class/File.Copy.cs
@@ -1049,14 +1049,15 @@ namespace Alphaleonis.Win32.Filesystem
       }
 
 
+      [SuppressMessage("Microsoft.Performance", "CA1820:TestForEmptyStringsUsingStringLength")]
       internal static void ValidateAndUpdatePathsAndOptions(KernelTransaction transaction, string sourcePath, string destinationPath, CopyOptions? copyOptions, MoveOptions? moveOptions, PathFormat pathFormat,
          out string sourcePathLp, out string destinationPathLp, out bool isCopy, out bool emulateMove, out bool delayUntilReboot, out bool deleteOnStartup)
       {
          if (sourcePath == string.Empty)
-            throw new ArgumentException("sourcePath");
+            throw new ArgumentException("Empty sourcePath name is not legal.");
 
          if (destinationPath == string.Empty)
-            throw new ArgumentException("destinationPath");
+            throw new ArgumentException("Empty destinationPath name is not legal.");
 
 
          // MSDN: .NET3.5+: IOException: The sourceDirName and destDirName parameters refer to the same file or directory.
